e101a8f3ff49dcad56a11525a0047709eb842d9b
f1erfly94 <99speedyz99@gmail.com>
2025-03-10 15:01:20 +0200

    fix issue navContext

--- app/context/NavContext.tsx
+++ app/context/NavContext.tsx
@@ -1,7 +1,12 @@
-'use client'
+'use client';
 import React, { createContext, useState, ReactNode } from 'react';
 
-export const NavContext = createContext<any>(null);
+interface NavContextType {
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const NavContext = createContext<NavContextType | null>(null);
 
 interface NavContextProviderProps {
     children: ReactNode;
