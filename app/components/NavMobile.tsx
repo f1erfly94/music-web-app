ba7f4b41ee5e0d74f01745a38b7ec8bb160b9ba0
f1erfly94 <99speedyz99@gmail.com>
2025-03-10 15:36:53 +0200

    fix issue albumsTracks

--- app/components/NavMobile.tsx
+++ app/components/NavMobile.tsx
@@ -4,17 +4,23 @@ import { RiCloseLine } from 'react-icons/ri';
 import Nav from "@/app/components/Nav";
 
 const NavMobile: React.FC = () => {
-    const { isOpen, setIsOpen } = useContext(NavContext);
+    const context = useContext(NavContext);
+
+    if (!context) {
+        return null;
+    }
+
+    const { isOpen, setIsOpen } = context;
 
     return (
         <nav className={`${isOpen ? 'right-0' : 'right-full'} 
             xl:hidden bg-[#1c060b] fixed w-full top-0 z-20 bottom-0 transition-all duration-500`}>
             <div
                 onClick={() => setIsOpen(!isOpen)}
-                className='absolute right-4 top-5 cursor-pointer '>
+                className='absolute right-4 top-5 cursor-pointer'>
                 <RiCloseLine className='text-5xl' />
             </div>
-            <Nav containerStyles="flex flex-col text-[30px] ppercase font-bold bg-[#4d0919 h-full items-center justify-center gap-y-8"/>
+            <Nav containerStyles="flex flex-col text-[30px] uppercase font-bold bg-[#4d0919] h-full items-center justify-center gap-y-8"/>
         </nav>
     );
 };
